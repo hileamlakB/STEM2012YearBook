b2378df2e5a71b585a3584eb0b453d5a78b41eaf
Hileamlak Yitayew <hileamlak@localhost.localdomain>
2022-01-22 17:02:22 -0800

    Add more students

--- script/studentData.js
+++ script/studentData.js
@@ -6,7 +6,7 @@ var data = [{
     "address":"Kebele 14 Efrata, Bahir Dar, Ethiopia",//
     "future":"Probably start studying medicine in A.A.U whenever that is. And hopefully drop out when I find what I actually want to do. So no accurate answer for me.",//
     "anythingElse":"Nope",//
-    "socialMedia":"edenkinde513@gmail.com , 0943544633",//
+    "socialMedia":"*****@gmail.com , **********",//
     "images":[
         ["Img/kid1.jpg",["Image discription","friend Notes"]],
         ["Img/kid2.jpg",["Image discription","friend Notes"]],
@@ -19,7 +19,75 @@ var data = [{
     "friendNotes": ["",""],//
     "theam":"rgba(192, 205, 214, 0.5)",
     "bgImage":"https://images.unsplash.com/photo-1505664194779-8beaceb93744?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
-},{
+},
+{
+    "fullName":"Eden Kinde Kebede",//
+    "quote":"Second person",//
+    "age":"",//
+    "schoolYear":"2016-2020",//
+    "address":"Kebele 14 Efrata, Bahir Dar, Ethiopia",//
+    "future":"Probably start studying medicine in A.A.U whenever that is. And hopefully drop out when I find what I actually want to do. So no accurate answer for me.",//
+    "anythingElse":"Nope",//
+    "socialMedia":"*****@gmail.com , **********",//
+    "images":[
+        ["Img/kid1.jpg",["Image discription","friend Notes"]],
+        ["Img/kid2.jpg",["Image discription","friend Notes"]],
+        ["Img/kid1.jpg",["Image discription","friend Notes"]],
+        ["Img/kid2.jpg",["Image discription","friend Notes"]],
+        ["Img/kid1.jpg",["Image discription","friend Notes"]],
+        ["Img/kid2.jpg",["Image discription","friend Notes"]],
+        ["Img/kid2.jpg",["Image discription","friend Notes"]]],//
+    "cartoonImage":"Img/KidusGecartoonImage.png",//
+    "friendNotes": ["",""],//
+    "theam":"rgba(192, 205, 214, 0.5)",
+    "bgImage":"https://images.unsplash.com/photo-1505664194779-8beaceb93744?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+},
+{
+    "fullName":"Eden Kinde Kebede",//
+    "quote":"Third person 😒",//
+    "age":"",//
+    "schoolYear":"2016-2020",//
+    "address":"Kebele 14 Efrata, Bahir Dar, Ethiopia",//
+    "future":"Probably start studying medicine in A.A.U whenever that is. And hopefully drop out when I find what I actually want to do. So no accurate answer for me.",//
+    "anythingElse":"Nope",//
+    "socialMedia":"*****@gmail.com , **********",//
+    "images":[
+        ["Img/kid1.jpg",["Image discription","friend Notes"]],
+        ["Img/kid2.jpg",["Image discription","friend Notes"]],
+        ["Img/kid1.jpg",["Image discription","friend Notes"]],
+        ["Img/kid2.jpg",["Image discription","friend Notes"]],
+        ["Img/kid1.jpg",["Image discription","friend Notes"]],
+        ["Img/kid2.jpg",["Image discription","friend Notes"]],
+        ["Img/kid2.jpg",["Image discription","friend Notes"]]],//
+    "cartoonImage":"Img/KidusGecartoonImage.png",//
+    "friendNotes": ["",""],//
+    "theam":"rgba(192, 205, 214, 0.5)",
+    "bgImage":"https://images.unsplash.com/photo-1505664194779-8beaceb93744?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+},
+{
+    "fullName":"Eden Kinde Kebede",//
+    "quote":"4th person 😒",//
+    "age":"18",//
+    "schoolYear":"2016-2020",//
+    "address":"Kebele 14 Efrata, Bahir Dar, Ethiopia",//
+    "future":"Probably start studying medicine in A.A.U whenever that is. And hopefully drop out when I find what I actually want to do. So no accurate answer for me.",//
+    "anythingElse":"Nope",//
+    "socialMedia":"*****@gmail.com , **********",//
+    "images":[
+        ["Img/kid1.jpg",["Image discription","friend Notes"]],
+        ["Img/kid2.jpg",["Image discription","friend Notes"]],
+        ["Img/kid1.jpg",["Image discription","friend Notes"]],
+        ["Img/kid2.jpg",["Image discription","friend Notes"]],
+        ["Img/kid1.jpg",["Image discription","friend Notes"]],
+        ["Img/kid2.jpg",["Image discription","friend Notes"]],
+        ["Img/kid2.jpg",["Image discription","friend Notes"]]],//
+    "cartoonImage":"Img/KidusGecartoonImage.png",//
+    "friendNotes": ["",""],//
+    "theam":"rgba(192, 205, 214, 0.5)",
+    "bgImage":"https://images.unsplash.com/photo-1505664194779-8beaceb93744?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+},
+
+{
     "fullName":"",//
     "quote":"MI casa estu Casa",//
     "age":"",//
